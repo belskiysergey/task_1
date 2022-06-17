9cc5be4a6ca5521fb857cf0a93db9e512e134fcc
Сергей Бельский <belskiysergey90@gmail.com>
2022-06-17 20:54:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Находим объём цилиндра. Пользователь может ввести в модальное окно только число больше нуля, 
+// в противном случае модальное окно с запросом данных будет вызвано снова.
+// Результат округления происходит до целого числа по математическим правилам: 3,45 - сначала будет 3,5 - потом 4
+
+var MATH_NUMBER_ZERO = 0;
+
+do {
+  var radius = prompt("Задайте значение радиуса цилиндра:");
+}
+while
+  ((isNaN(radius))
+  || (radius === " ")
+  || (radius === "")
+  || (radius <= MATH_NUMBER_ZERO));
+
+do {
+  var cylinderHeight = prompt("Задайте значение высоты цилиндра:");
+}
+while
+  ((isNaN(cylinderHeight))
+  || (cylinderHeight === " ")
+  || (cylinderHeight === "")
+  || (cylinderHeight <= MATH_NUMBER_ZERO));
+
+let fullVolumeCylinder = Math.PI * Math.pow(radius, 2) * cylinderHeight;
+let result = Math.round(fullVolumeCylinder.toFixed(1));
+
+alert("Радиус: " + radius + "\n" +
+  "Высота: " + cylinderHeight + "\n" +
+  "Объём цилиндра: " + result);
